109802d16fac442b5a23c89e1c68ad8ca12bf45e
Muhammad <umedjon02.ud@gmail.com>
2023-03-17 21:17:23 +0500

    Update style.css .added media queries

--- src/index.js
+++ src/index.js
@@ -7,15 +7,32 @@ const user = document.querySelector('.name');
 const score = document.querySelector('.score');
 const scoreTable = document.querySelector('.score-history');
 
+const toast = Swal.mixin({
+  toast: true,
+  icon: 'success',
+  title: 'General Title',
+  position: 'top-right',
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+});
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   post(user, score);
+  toast.fire({
+    title: 'Score added successfully!',
+  });
 });
 
 refresh.addEventListener('click', () => {
   update(scoreTable);
+  toast.fire({
+    title: 'Refreshed successfully!',
+  });
 });
 
 window.addEventListener('DOMContentLoaded', () => {
   update(scoreTable);
 });
+
